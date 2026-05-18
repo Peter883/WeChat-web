a63f05d473f9f89df26d88314e26fb60e97fc485
Peter883 <759915898@qq.com>
2026-05-18 12:26:12 +0800

    Change title and message in deploy_wechat.py

--- deploy_wechat.py
+++ deploy_wechat.py
@@ -22,7 +22,7 @@ def generate_html():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <title>生日祝福</title>
+    <title>祝福</title>
     <style>
         html, body {
             height: 100%;
@@ -47,7 +47,7 @@ def generate_html():
 </head>
 <body>
     <div>
-        蓓妮生日快乐<span class="emoji">🎂</span>
+        天天开心吖<span class="emoji">[坏笑]</span>
     </div>
 </body>
 </html>
